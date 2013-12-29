dee5a20982393e34fe50ec9fb20277f250afc6b6
ewokcillo <azotillo@gmail.com>
2013-12-29 13:19:53 +0100

    added birth place to unavrra crawler

---
+++ vidascontadas/places/migrations/0003_auto__chg_field_place_city.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Place.city'
+        db.alter_column(u'places_place', 'city_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cities_light.City'], null=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'Place.city'
+        db.alter_column(u'places_place', 'city_id', self.gf('django.db.models.fields.related.ForeignKey')(default=None, to=orm['cities_light.City']))
+
+    models = {
+        u'cities_light.city': {
+            'Meta': {'ordering': "['name']", 'unique_together': "(('region', 'name'),)", 'object_name': 'City'},
+            'alternate_names': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cities_light.Country']"}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'feature_code': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'geoname_id': ('django.db.models.fields.IntegerField', [], {'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '8', 'decimal_places': '5', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '8', 'decimal_places': '5', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'db_index': 'True'}),
+            'name_ascii': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '200', 'blank': 'True'}),
+            'population': ('django.db.models.fields.BigIntegerField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
+            'region': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cities_light.Region']", 'null': 'True', 'blank': 'True'}),
+            'search_names': ('cities_light.models.ToSearchTextField', [], {'default': "''", 'max_length': '4000', 'db_index': 'True', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique_with': '()', 'max_length': '50', 'populate_from': "'name_ascii'"})
+        },
+        u'cities_light.country': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Country'},
+            'alternate_names': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'code2': ('django.db.models.fields.CharField', [], {'max_length': '2', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'code3': ('django.db.models.fields.CharField', [], {'max_length': '3', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'continent': ('django.db.models.fields.CharField', [], {'max_length': '2', 'db_index': 'True'}),
+            'geoname_id': ('django.db.models.fields.IntegerField', [], {'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'}),
+            'name_ascii': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '200', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique_with': '()', 'max_length': '50', 'populate_from': "'name_ascii'"}),
+            'tld': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '5', 'blank': 'True'})
+        },
+        u'cities_light.region': {
+            'Meta': {'ordering': "['name']", 'unique_together': "(('country', 'name'),)", 'object_name': 'Region'},
+            'alternate_names': ('django.db.models.fields.TextField', [], {'default': "''", 'null': 'True', 'blank': 'True'}),
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cities_light.Country']"}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'geoname_code': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'geoname_id': ('django.db.models.fields.IntegerField', [], {'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200', 'db_index': 'True'}),
+            'name_ascii': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '200', 'blank': 'True'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique_with': '()', 'max_length': '50', 'populate_from': "'name_ascii'"})
+        },
+        u'medias.media': {
+            'Meta': {'object_name': 'Media'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'media_file': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'media_type': ('django.db.models.fields.CharField', [], {'default': "'image'", 'max_length': '50'})
+        },
+        u'places.place': {
+            'Meta': {'object_name': 'Place'},
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cities_light.City']", 'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '5', 'blank': 'True'}),
+            'longitude': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '5', 'blank': 'True'}),
+            'medias': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['medias.Media']", 'symmetrical': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'place_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['places']
--- vidascontadas/scrap/spiders/base.py
+++ vidascontadas/scrap/spiders/base.py
@@ -4,6 +4,8 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
 import scrap.items as items
 from common.models import CustomDate
+from cities_light.models import City
+from places.models import Place
 
 from bs4 import BeautifulSoup
 
@@ -13,6 +15,65 @@ import re
 def get_int_from_text(expression, sub, text):
     re_result = re.search(expression, text, re.M|re.I|re.U)
     return int(re.sub(sub, '', re_result.group())) if re_result else 0
+
+def get_place_from_text(place_html):
+    city_query = City.objects
+
+    #region
+    region_html = place_html.findNext(text=re.compile('Provincia: \w+'))
+    region_text = re.sub('Provincia: ', '', region_html)
+    if not region_text == u'No informa/Ez du informatzen':
+        city_query = city_query.filter(region__name__iexact=region_text)
+        if not city_query:
+            pass
+            #TODO
+            #send mail with data to add to db
+
+    #city
+    city_html = place_html.find_next(text=re.compile('Municipio: \w+'))
+    city_text = re.sub('Municipio: ', '', city_html)
+    if not city_text == u'No informa/Ez du informatzen':
+        city_query = city_query.filter(name__iexact=city_text)
+        if not city_query:
+            pass
+            #TODO
+            #send mail with data to add to db
+
+    city = None
+    if city_query != City.objects and city_query:
+        if len(city_query) > 1:
+            pass
+            #TODO
+            #send mail to detect duplication cities
+        else:
+            city = city_query[0]
+
+    name = ''
+    #neighbourhood
+    neighbourhood_html = place_html.find_next(
+                         text=re.compile('Concejo o barrio: \w+'))
+    neighbourhood_text = re.sub('Concejo o barrio: ', '', neighbourhood_html)
+    if not neighbourhood_text == u'No informa/Ez du informatzen':
+        name += neighbourhood_text
+
+    #subregion
+    subregion_html = place_html.find_next(
+                         text=re.compile('Comarca: \w+'))
+    subregion_text = re.sub('Comarca: ', '', subregion_html)
+    if not subregion_text == u'No informa/Ez du informatzen':
+        name += subregion_text
+
+    #get place
+    places = Place.objects
+
+    places = places.filter(city=city) if city else places.none()
+
+    if name != '' or places:
+        place, created = places.get_or_create(name=name)
+        place.save()
+        return place
+    else:
+        return None
 
 
 class UnavarraSpider(CrawlSpider):
@@ -62,11 +123,13 @@ class UnavarraSpider(CrawlSpider):
         #Age
         item['age'] = get_int_from_text(u'Edad: \d{1,3}','Edad: ',ficha.text)
 
-        import ipdb;ipdb.set_trace()
+        #birth places
+        birth_place_html = ficha.find(text='Lugar de nacimiento')
+        item['place_birth'] = get_place_from_text(birth_place_html)
 
         item.save()
 
-        print item.fields
+        print item['place_birth']
 
         return item
 
